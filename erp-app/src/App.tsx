444b62492038c96d06843f6e98b8b9ed8ea5c4ae
ΤοΌνομάΣου <email@.com>
2026-08-02 18:32:36 +0300

    feat: add initial application routing

--- erp-app/src/App.tsx
+++ erp-app/src/App.tsx
@@ -1,15 +1,20 @@
 import Layout from "./shared/Layout/Layout";
+import { Route , Routes} from "react-router"
+import DashboardPage from "./features/dashboard/DashboardPage"
+import ProductsPage from "./features/products/ProductsPage"
 
 function App() {
     return (
-        <>
         <Layout>
-            <h1>Dashboard</h1>
+            <Routes>
+                <Route path="/" element={<DashboardPage/>} />
+                <Route path="/products" element={<ProductsPage/>} />
+            </Routes>
         </Layout>
 
 
 
-        </>
+
 
     )
 }
---
+++ erp-app/src/features/dashboard/DashboardPage.tsx
@@ -0,0 +1,8 @@
+const DashboardPage = () => {
+    return (
+        <>
+            <h2>Dashboard</h2>
+        </>
+    )
+}
+export default DashboardPage;
